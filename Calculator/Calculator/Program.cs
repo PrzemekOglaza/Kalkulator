b8519cb54234b82de1edade7c72ad9d0a3b1eddc
Przemek O <przemyslaw.oglaza@outlook.com>
2019-09-05 15:40:24 +0200

    Kalkulator - mozliwość wpisania liczb przez urzytkownika oraz wybrania operacji

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -5,11 +5,51 @@ namespace Calculator
     class Program
     {
         static void Main(string[] args)
-        {
-            int a = 42;
-            int b = 119;
-            int c = a + b;
-            Console.WriteLine(c);
+        {   //Zadeklaruj zmienne, a następnie zainicjuj do zera.
+            int num1 = 0;
+            int num2 = 0;
+
+            //Wyświetl tytuł jako aplikację kalkulatora konsoli C #.
+
+            Console.WriteLine("Aplikacja kalkulatora dla C#\r");
+            Console.WriteLine("----------------------------\n");
+
+            //Poproś użytkownika o wpisanie liczb.
+
+            Console.WriteLine("Wprowadz pierwszą liczbę i naciśnij ENTER");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Wprowadz drugą liczbę i naciśnij ENTER");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            //Wybierz opcje z listy
+
+            Console.WriteLine("Wybierz z listy i zatwierdz ENTER: ");
+            Console.WriteLine("a - Add (DODAJ)");
+            Console.WriteLine("s - Subtract (ODEJMIJ)");
+            Console.WriteLine("m - Multiply (POMNÓŻ)");
+            Console.WriteLine("d- Divide (PODZIEL)");
+            Console.Write("Twój wybór?  ");
+
+            //Instrukcja SWITCH do obliczeń matematycznych
+
+            switch (Console.ReadLine())
+            {
+                case "a":
+                    Console.WriteLine($"Twoje równanie: {num1} + {num2} = "+(num1+num2));
+                    break;
+                case "s":
+                    Console.WriteLine($"Twoje równanie: {num1} - {num2} = "+( num1 - num2));
+                    break;
+                case "m":
+                    Console.WriteLine($"Twoje równanie: {num1} * {num2} = "+(num1 * num2));
+                    break;
+                case "d":
+                    Console.WriteLine($"Twoje równanie: {num1} / {num2} = "+(num1/num2));
+                    break;
+            }
+            //Zamknięcie Kalkulatora
+            Console.WriteLine("Wciśnij dowolny klawisz żeby zakończyć pracę kalkulatora");
             Console.ReadKey();
         }
     }
